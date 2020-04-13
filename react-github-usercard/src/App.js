7487d46351f444fa1b01ab752f570aaefef0a542
Darrell-Conklin <dmconklin84@gmail.com>
2020-04-12 22:36:34 -0500

    fetch github api data on componentDidMount

--- react-github-usercard/src/App.js
+++ react-github-usercard/src/App.js
@@ -1,26 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state= {
+      user: {}
+    }
+  }
+
+  componentDidMount = () => {
+    fetch(`https://api.github.com/users/DMConklin`, {
+      headers: {
+        'Authorization': 'token ce083e3f2646a24506e20ce66e96e17f726fd74a',
+        'Accept': 'application/vnd.github.v3+json'
+      }
+    })
+    .then(res => console.log(res.json()))
+    .catch(err => console.log(err))
+  }
+  
+
+  render() {
+    return (
+        <div className="App">
+          
+        </div>
+    );
+  }
 }
 
 export default App;
